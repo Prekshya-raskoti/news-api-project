1679decd87488f4b4c77193edb3290dbc737ba30
Prekshya-raskoti <bisuraskoti@gmail.com>
2026-05-27 16:45:00 +0545

    Initial commit - News API project

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    print("Program started")
+import requests
+
+api_key = "d929dbe211874dc29109a493cb72c0f8"
+
+url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
+
+response = requests.get(url)
+news = response.json()
+
+articles = news["articles"]
+
+for index, article in enumerate(articles, start=1):
+    print(f"{index}. {article['title']}")
